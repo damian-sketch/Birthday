e37b70f805548d2baf5af2b5c7396c6e1d4a52eb
Cedric Damian <cedricdamian19@gmail.com>
2020-07-17 15:20:31 +0300

    Add calculating Functionality

--- birthday.js
+++ birthday.js
@@ -1,8 +1,24 @@
-//Get's the user's Year of Birth and retrieves the first two digits
+function calculate(){
+  var YOB = document.getElementById('YOB').value;
+  var Century = YOB.slice(0,2);
+  var CC = parseInt(Century);
+  var Year = YOB.slice(2,4);
+  var YY = parseInt(Year);
 
-var year =prompt('Enter your age here');
 
-var CCs = year.slice(0, 2);
+  var MOB = document.getElementById('MOB').value;
+  var MM = parseInt(MOB);
+  if (MM<=0 || MM>12){
+    alert('Enter a valid month');
+  }
 
-var CC = parseInt(CCs);
-alert(CC);
+  var DOB = document.getElementById('DOB').value;
+  var DD = parseInt(DOB);
+  if (DD<=0 || DD>31){
+    alert('Enter a valid Date');
+  }
+
+  var result = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7
+
+  alert(result);
+}
